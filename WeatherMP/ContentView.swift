dc50c472d09607e4993190d8a93378b50b5ede31
Alexander Kraev <lexkraev@gmail.com>
2020-07-02 22:42:13 +0300

    Initial Commit

---
+++ WeatherMP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherMP
+//
+//  Created by Alexander Kraev on 02.07.2020.
+//  Copyright © 2020 Alexander Kraev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
